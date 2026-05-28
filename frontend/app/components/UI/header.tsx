5f66faa06c2c0b3f9e522757c0e99e17299f8b3d
MattiasKopparberg <mkopparberg@hotmail.com>
2026-05-28 13:36:42 +0200

    Split front- and backend

---
+++ frontend/app/components/UI/header.tsx
@@ -0,0 +1,15 @@
+import Button from "@/components/UI/button"
+
+
+export default function Header() {
+    <div>
+        <ul>
+            <Button>
+                Home
+            </Button>
+            <Button>
+                Login
+            </Button>
+        </ul>
+    </div>
+} 
